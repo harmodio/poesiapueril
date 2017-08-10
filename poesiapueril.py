e5dcd2ddb765b71598afcb46fcbc6c0c5213ce65
Jorge Garcia Flores <jorgegarciaflo@gmail.com>
2017-08-10 13:50:48 -0500

    initial

---
+++ poesiapueril.py
@@ -0,0 +1,35 @@
+import tweepy
+from time import sleep
+
+#Create variables for each key, secret, token
+consumer_key = 'PzwW4qupC1qy3562g2wtCfMV1'
+consumer_secret = 'wCivK4jYECDUbJCQBn9294EtGsYAgk2Z5V8wyLFM31tNjqNypW'
+access_token = '869611298547421184-PfE7owxRC1RLC7bgzp14jdjvCcyjpyV'
+access_token_secret = 'aU4r2FCuZtU5d6tR1LbfAbXb0r7MlcejbEso7OlSax7jI'
+ 
+#Setup OAuth and integrate with API 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+#Write a tweet to push to our @poesiapueril account
+#tweet = 'hola\nsoy\nun\nbot\nde\n#poesíapueril'
+#api.update_status(status=tweet)
+
+for tweet in tweepy.Cursor(api.search, q='#poesíapueril').items():
+    try:
+        #Add \n escape character to print() to organize tweets
+        print('\Tweet by @' + tweet.user.screen_name)
+
+        #Retweets tweets as they are found
+        tweet.retweet()
+        print('Retweeted the tweet')
+
+        sleep(300)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+
+    except StopIteration:
+        break
+
